1a55d01b606493042765f97746b2001b7d172cfe
GYUBIN CHOI <122433920+gyubinc@users.noreply.github.com>
2025-11-08 02:44:35 +0900

    Time: 1 ms (32.51%), Space: 10.5 MB (38.81%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.c
@@ -0,0 +1,24 @@
+int romanToInt(char* s) {
+    int i=0;
+    int ans = 0;
+    while (s[i] != '\0'){
+        if (s[i] == 'M') ans += 1000;
+        else if (s[i] == 'D') ans += 500;
+        else if (s[i] == 'C') {
+            if (s[i+1] == 'D' || s[i+1] == 'M') ans -= 100;
+            else ans += 100;
+        }
+        else if (s[i] == 'L') ans += 50;
+        else if (s[i] == 'X') {
+            if (s[i+1] == 'L' || s[i+1] == 'C') ans -= 10;
+            else ans += 10;
+        }
+        else if (s[i] == 'V') ans += 5;
+        else if (s[i] == 'I') {
+            if (s[i+1] == 'V' || s[i+1] == 'X') ans -= 1;
+            else ans += 1;
+        }
+        i++;
+    }
+    return ans;
+}
