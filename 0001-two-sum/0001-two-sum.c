74307dd0a76c3e067540246f2ce379512ab12cc9
GYUBIN CHOI <122433920+gyubinc@users.noreply.github.com>
2025-11-08 13:53:00 +0900

    Time: 103 ms (33.85%), Space: 8.5 MB (92.83%) - LeetHub

--- 0001-two-sum/0001-two-sum.c
+++ 0001-two-sum/0001-two-sum.c
@@ -6,8 +6,7 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     int* ans = (int*)malloc(sizeof(int) * 2);
     
     for (int i=0; i<numsSize; i++){
-        for (int j=0; j<numsSize; j++){
-            if (i != j || i < j){
+        for (int j=i+1; j<numsSize; j++){
                 if (nums[i] + nums[j] == target){
                     ans[0] = i;
                     ans[1] = j;
@@ -15,8 +14,5 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
                 }
             }
         }
+    return NULL;
     }
-
-
-    return ans;
-}
